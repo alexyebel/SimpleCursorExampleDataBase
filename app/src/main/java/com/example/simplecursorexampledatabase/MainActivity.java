5ac21ee4b15e80b0d8d9722e839d90ffc8ec2f77
alexey <alexey.b@gmail.com>
2019-09-20 11:36:07 +0300

    ----2----

--- app/src/main/java/com/example/simplecursorexampledatabase/MainActivity.java
+++ app/src/main/java/com/example/simplecursorexampledatabase/MainActivity.java
@@ -1,62 +1,100 @@
 package com.example.simplecursorexampledatabase;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Build;
-import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    final int CONTACTS_REQUEST_CODE=1;
-    ArrayAdapter<String> adapter;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    final int CONTACTS_REQUEST_CODE = 1;
+    SimpleCursorAdapter adapter;
+    ListView contactsListView;
+    Cursor cursor;
+
+    String[] cols = {ContactsContract.CommonDataKinds.Phone._ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+
+    final int LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView contactListView=findViewById(R.id.contacts_list);
-        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
-        contactListView.setAdapter(adapter);
+        contactsListView = findViewById(R.id.contacts_list);
 
-        if(Build.VERSION.SDK_INT>=23){
-            int hasContactsPermission=checkSelfPermission(Manifest.permission.READ_CONTACTS);
-            if(hasContactsPermission== PackageManager.PERMISSION_DENIED){
+        if(Build.VERSION.SDK_INT>=23) {
+            int hasContactsPermission  = checkSelfPermission(Manifest.permission.READ_CONTACTS);
+            if(hasContactsPermission != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},CONTACTS_REQUEST_CODE);
             }
             else readContacts();
         }
         else readContacts();
+
+        //  startManagingCursor(cursor);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==CONTACTS_REQUEST_CODE&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        if(requestCode==CONTACTS_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             readContacts();
-        else
-            Toast.makeText(this, "Must give permission", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "Must give permission", Toast.LENGTH_SHORT).show();
     }
 
-    private void readContacts(){
-        //getContentResolver is android dataBase
-        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
-        int nameIndex=cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        int phoneIndex=cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+    private void readContacts() {
 
-        while (cursor.moveToNext()){
-            String contact=cursor.getString(nameIndex)+" "+ cursor.getString(phoneIndex);
-            adapter.add(contact);
+
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+        int[] to = {android.R.id.text1,android.R.id.text2};
+
+        // cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,cols,null,null,null);
+
+        adapter = new SimpleCursorAdapter(this,android.R.layout.simple_expandable_list_item_2,null,from,to,0);
+
+        contactsListView.setAdapter(adapter);
+
+        getLoaderManager().initLoader(LOADER_ID,null,this);
+        //cursor.close();
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,ContactsContract.CommonDataKinds.Phone.CONTENT_URI,cols,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+        if(loader.getId() == LOADER_ID) {
+            adapter.swapCursor(cursor);
         }
-        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //stopManagingCursor(cursor);
     }
 }
